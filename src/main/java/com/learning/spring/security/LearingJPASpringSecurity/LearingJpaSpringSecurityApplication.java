6d130ff2ebf4e46b6b3702241766f855ea26efe3
Anurag <anuragsingh.anurag1234@gmail.com>
2025-06-07 23:09:42 +0530

    adding spring boot project

---
+++ src/main/java/com/learning/spring/security/LearingJPASpringSecurity/LearingJpaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.security.LearingJPASpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingJpaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingJpaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring/security/LearingJPASpringSecurity/LearingJpaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.security.LearingJPASpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingJpaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
