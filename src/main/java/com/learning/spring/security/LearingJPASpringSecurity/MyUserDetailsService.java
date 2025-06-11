77176306978257bbfe335f582da3baee0ff19af8
Anurag <anuragsingh.anurag1234@gmail.com>
2025-06-11 12:18:10 +0530

    Adding Jpa configuration

--- src/main/java/com/learning/spring/security/LearingJPASpringSecurity/MyUserDetailsService.java
+++ src/main/java/com/learning/spring/security/LearingJPASpringSecurity/MyUserDetailsService.java
@@ -1,17 +1,31 @@
 package com.learning.spring.security.LearingJPASpringSecurity;
 
 import com.learning.spring.security.LearingJPASpringSecurity.models.MyUserDetails;
+import com.learning.spring.security.LearingJPASpringSecurity.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
+
+
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Username not found: "+ userName));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
